71625a19cda32c478f137b4ed0ca6c815a2ee65c
danygagnon <ddanygagnon@gmail.com>
2021-09-12 15:53:14 -0400

    refactor: cleaned code

--- js/index.js
+++ js/index.js
@@ -1,36 +1,37 @@
-const createCharacterEntry = (character) => `
+const createCharacterEntry = character =>
+    createListItem(`
     <img class="w-md" src="${character.img}" alt="${character.nom}">
     <button type="button" onclick="handleClick(this)" data-message="${character.attaque()}">
         <img class="w-xs" src="asset/attack.jpg" alt="attack">
     </button>
     <button type="button" onclick="handleClick(this)" data-message="${character.support()}">
         <img class="w-xs" src="asset/support.jpg" alt="support">
-    </button>`;
+    </button>`);
 
-const createListItem = (html) => {
-  const li = document.createElement("li");
-  li.className = "list-group-item";
-  li.innerHTML = html;
-  return li;
+const createListItem = html => {
+    const li = document.createElement("li");
+    li.className = "list-group-item";
+    li.innerHTML = html;
+    return li;
 };
 
 function handleClick({ dataset: { message } }) {
-  const battlelog = document.getElementById("battlelog");
+    const battlelog = document.getElementById("battlelog");
 
-  const listItem = createListItem(message);
-  battlelog.appendChild(listItem);
+    const listItem = createListItem(message);
+    battlelog.appendChild(listItem);
 }
 
 function afficherPersonnage(personnage) {
-  const characters = document.getElementById("characters");
+    const characters = document.getElementById("characters");
 
-  const listItem = createListItem(createCharacterEntry(personnage));
-  characters.appendChild(listItem);
+    const listItem = createCharacterEntry(personnage);
+    characters.appendChild(listItem);
 }
 
 window.onload = () => {
-  afficherPersonnage(new Wizard());
-  afficherPersonnage(new Rogue());
-  afficherPersonnage(new Fighter());
-  afficherPersonnage(new Monk());
+    afficherPersonnage(new Wizard());
+    afficherPersonnage(new Rogue());
+    afficherPersonnage(new Fighter());
+    afficherPersonnage(new Monk());
 };
